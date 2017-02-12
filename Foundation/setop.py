ccc4cf7be554e8d8f87f042771a24bc7ab4924ad
Zhang Chang-kai <phy.zhangck@gmail.com>
2017-02-12 14:19:52 +0000

    Complete Foundation.setop draft

--- Foundation/setop.py
+++ Foundation/setop.py
@@ -8,101 +8,134 @@
 
 
 from Elementary.certify import Check
-from Foundation.basic import MathBasic
-from Foundation.initio import Variable, BelongTo
-from Foundation.logic import Negation, \
-    Conjunction, Disjunction
+from Foundation.basic import Variable, Operator, \
+    Morphism
+from Foundation.logic import Neg, Conjunc, Disjunc
+from Foundation.set import Subclass, Set, \
+    Element, SetEqual
 
 
-class Union(MathBasic):
+class Union(Operator):
 
-    Definition = "Union"
-    Denotation = "\\cup"
+    _Define = "Union"
+    _Symbol = "\\cup"
 
-    @classmethod
-    def Union(Cls, Left, Rght):
+    @staticmethod
+    def Action(self, Left, Rght):
         Check(Left, Set)
         Check(Rght, Set)
 
-        def Condition(InVar, InClass):
-            return Disjunction.Disjunc(
-                BelongTo.BelongsTo \
-                    (TempSet.Elmnt, Left), \
-                BelongTo.BelongsTo \
-                    (TempSet.Elmnt, Rght)).Truth
+        def Condition(InVar, InSet):
+            return Disjunc(
+                InVar.BelongTo(InSet\
+                    .Unique['Depend'][0]),
+                InVar.BelongTo(InSet\
+                    .Unique['Depend'][1])
+            ).Truth
 
         TempSet = Set()
-        TempSet.MathForm = Left.Symbol + \
-            Cls.Symbol + Rght.Symbol
-        TempSet.PropForm =
-            Disjunction.Disjunc( \
-                BelongTo.BelongsTo \
-                    (TempSet.Elmnt, Left), \
-                BelongTo.BelongsTo \
-                    (TempSet.Elmnt, Rght)) \
-            .MathForm
+        TempSet.Unique['Depend']\
+            .extend([Left, Rght])
+        TempSet.Format = lambda self: \
+            TempSet.Unique['Depend'][0].Symbol \
+                + self.Symbol + \
+            TempSet.Unique['Depend'][1].Symbol
+        TempSet.PropForm = lambda self: Disjunc(
+            InVar.BelongTo(InSet\
+                .Unique['Depend'][0]),
+            InVar.BelongTo(InSet\
+                .Unique['Depend'][1])).Format
         TempSet.Condition = Condition
+        return TempSet
 
 
-class Intersection(MathBasic):
+class Intersection(Operator):
 
-    Definition = "Intersection"
-    Denotation = "\\cap"
+    _Define = "Intersection"
+    _Symbol = "\\cap"
 
-    @classmethod
-    def Intersect(Cls, Left, Rght):
+    @staticmethod
+    def Action(self, Left, Rght):
         Check(Left, Set)
         Check(Rght, Set)
 
-        def Condition(InVar, InClass):
-            return Conjunction.Conjunc(
-                BelongTo.BelongsTo \
-                    (TempSet.Elmnt, Left), \
-                BelongTo.BelongsTo \
-                    (TempSet.Elmnt, Rght)).Truth
+        def Condition(InVar, InSet):
+            return Conjunc(
+                InVar.BelongTo(InSet\
+                    .Unique['Depend'][0]),
+                InVar.BelongTo(InSet\
+                    .Unique['Depend'][1])
+            ).Truth
 
         TempSet = Set()
-        TempSet.MathForm = Left.Symbol + \
-            Cls.Symbol + Rght.Symbol
-        TempSet.PropForm =
-            Conjunction.Conjunc( \
-                BelongTo.BelongsTo \
-                    (TempSet.Elmnt, Left), \
-                BelongTo.BelongsTo \
-                    (TempSet.Elmnt, Rght)) \
-            .MathForm
+        TempSet.Unique['Depend']\
+            .extend([Left, Rght])
+        TempSet.Format = lambda self: \
+            TempSet.Unique['Depend'][0].Symbol \
+                + self.Symbol + \
+            TempSet.Unique['Depend'][1].Symbol
+        TempSet.PropForm = lambda self: Conjunc(
+            InVar.BelongTo(InSet\
+                .Unique['Depend'][0]),
+            InVar.BelongTo(InSet\
+                .Unique['Depend'][1])).Format
         TempSet.Condition = Condition
+        return TempSet
 
 
-class Complement(MathBasic):
+class Complement(Operator):
 
-    Definition = "Complement"
-    Denotation = "\\complement"
+    _Define = "Complement"
+    _Symbol = "\\complement"
 
-    @classmethod
-    def Complt(Cls, Input, Univ):
-        Check(Input, Set)
-        Check(Univ, Set)
+    @staticmethod
+    def Action(self, Univ, Input):
+        Check(Left, Univ)
+        Check(Rght, Input)
 
-        def Condition(InVar, InClass):
-            return Conjunction.Conjunc(
-                Negation.Neg(
-                BelongTo.BelongsTo \
-                    (TempSet.Elmnt, Input)), \
-                BelongTo.BelongsTo \
-                    (TempSet.Elmnt, Univ)).Truth
+        def Condition(InVar, InSet):
+            return Conjunc(
+                InVar.BelongTo(InSet\
+                    .Unique['Depend'][0]),
+                Neg(InVar.BelongTo(InSet\
+                    .Unique['Depend'][1]))
+            ).Truth
 
         TempSet = Set()
-        TempSet.MathForm = Left.Symbol + \
-            Cls.Symbol + Rght.Symbol
-        TempSet.PropForm =
-            Conjunction.Conjunc(
-                Negation.Neg(
-                BelongTo.BelongsTo \
-                    (TempSet.Elmnt, Input)), \
-                BelongTo.BelongsTo \
-                    (TempSet.Elmnt, Univ)) \
-            .MathForm
+        TempSet.Unique['Depend']\
+            .extend([Univ, Input])
+        TempSet.Format = lambda self: \
+            self.Symbol + "_" + \
+            TempSet.Unique['Depend'][0].Symbol + \
+            TempSet.Unique['Depend'][1].Symbol
+        TempSet.PropForm = lambda self: Conjunc(
+            InVar.BelongTo(InSet\
+                .Unique['Depend'][0]),
+            Neg(InVar.BelongTo(InSet\
+                .Unique['Depend'][1]))).Format
         TempSet.Condition = Condition
+        return TempSet
+
+
+def Contain(Left, Rght):
+    OpContain = Subclass()
+    return OpContain(Left, Rght)
+
+def SetEq(Left, Rght):
+    OpSetEq = SetEqual()
+    return OpSetEq(Left, Rght)
+
+def Union(Left, Rght):
+    OpUnion = Union()
+    return OpUnion(Left, Rght)
+
+def Intsct(Left, Rght):
+    OpIntersect = Intersection()
+    return OpIntersect(Left, Rght)
+
+def Complt(Univ, Input):
+    OpComplt = Complement()
+    return OpComplt(Univ, Input)
+
 
 # End of Module Foundation.setop of UnivMathSys
